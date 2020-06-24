72626dec2c373c9ddfcdbcfc991994cf679db7a0
Chris Monzert <cmonzert@gmail.com>
2020-06-24 17:36:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -45,3 +45,15 @@ horiz = "-----------"
   puts "#{horiz}"
   puts "#{board[6]}#{vert}#{board[7]}#{vert}#{board[8]}"
 end
+
+board = ["   ","   ","   ","   ","   ","   "," O "," O "," O "] 
+
+def display_board(board)
+vert = "|"
+horiz = "-----------"
+  puts "#{board[0]}#{vert}#{board[1]}#{vert}#{board[2]}"
+  puts "#{horiz}"
+  puts "#{board[3]}#{vert}#{board[4]}#{vert}#{board[5]}"
+  puts "#{horiz}"
+  puts "#{board[6]}#{vert}#{board[7]}#{vert}#{board[8]}"
+end
