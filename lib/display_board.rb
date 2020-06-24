04cdeaeb3e751ceb8eb96bff7207f794992af816
Chris Monzert <cmonzert@gmail.com>
2020-06-24 15:30:46 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,5 +5,12 @@ row1 = board[0], vert, board[1], vert, board[2]
 row2 = horiz
 row3 = board[3], vert, board[4], vert, board[5]
 row4 = horiz
-row5 = 
-  
+row5 = board[6], vert, board[7], vert, board[8]
+
+def display_board(board)
+  puts row1
+  puts row2
+  puts row3
+  puts row4
+  puts row5
+end
