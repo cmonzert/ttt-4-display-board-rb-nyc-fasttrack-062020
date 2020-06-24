df8718769b4b574e6b9aaef697483c31f47321c1
Chris Monzert <cmonzert@gmail.com>
2020-06-24 15:44:46 +0000

    Automatically backed up by Learn

---
+++ lib/display_board.rb
@@ -0,0 +1,10 @@
+def display_board(board)
+separator = "|"
+irb(main):100:1*   lines = "-----------"
+irb(main):101:1*   
+irb(main):102:1*   puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+irb(main):103:1*   puts "#{lines}"
+irb(main):104:1*   puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+irb(main):105:1*   puts "#{lines}"
+irb(main):106:1*   puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
+irb(main):107:0> end
