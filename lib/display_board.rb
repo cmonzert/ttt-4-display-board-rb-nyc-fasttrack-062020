d5b2945b565a5c112cbf09b381dd8d3ac0c04402
Chris Monzert <cmonzert@gmail.com>
2020-06-24 15:38:24 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 vert = "|"
 horiz = "-----------"
-row1 = "#{board[0]}, vert, board[1], vert, board[2]
-row2 = horiz
-row3 = board[3], vert, board[4], vert, board[5]
-row4 = horiz
-row5 = board[6], vert, board[7], vert, board[8]
+row1 = "#{board[0]}, #{vert}, #{board[1]}, #{vert}, #{board[2]}"
+row2 = "#{horiz}"
+row3 = "#{board[3]}, #{vert}, #{board[4]}, #{vert}, #{board[5]}"
+row4 = "#{horiz}"
+row5 = "#{board[6]}, #{vert}, #{board[7]}, #{vert}, #{board[8]}"
 
 def display_board(board)
   puts row1
