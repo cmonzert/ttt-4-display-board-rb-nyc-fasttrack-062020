c36c1177e1e4f61cdb28609edb39c7f5277fbc5c
Chris Monzert <cmonzert@gmail.com>
2020-06-24 17:36:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,51 +10,5 @@ horiz = "-----------"
   puts "#{board[6]}#{vert}#{board[7]}#{vert}#{board[8]}"
 end
 
-board = [" O ","   ","   ","   ","   ","   ","   ","   ","   "] 
 
-def display_board(board)
-vert = "|"
-horiz = "-----------"
-  puts "#{board[0]}#{vert}#{board[1]}#{vert}#{board[2]}"
-  puts "#{horiz}"
-  puts "#{board[3]}#{vert}#{board[4]}#{vert}#{board[5]}"
-  puts "#{horiz}"
-  puts "#{board[6]}#{vert}#{board[7]}#{vert}#{board[8]}"
-end
-
-board = [" O ","   ","   ","   "," X ","   ","   ","   ","   "] 
-
-def display_board(board)
-vert = "|"
-horiz = "-----------"
-  puts "#{board[0]}#{vert}#{board[1]}#{vert}#{board[2]}"
-  puts "#{horiz}"
-  puts "#{board[3]}#{vert}#{board[4]}#{vert}#{board[5]}"
-  puts "#{horiz}"
-  puts "#{board[6]}#{vert}#{board[7]}#{vert}#{board[8]}"
-end
-
-board = [" X "," X "," X ","   ","   ","   ","   ","   ","   "] 
-
-def display_board(board)
-vert = "|"
-horiz = "-----------"
-  puts "#{board[0]}#{vert}#{board[1]}#{vert}#{board[2]}"
-  puts "#{horiz}"
-  puts "#{board[3]}#{vert}#{board[4]}#{vert}#{board[5]}"
-  puts "#{horiz}"
-  puts "#{board[6]}#{vert}#{board[7]}#{vert}#{board[8]}"
-end
-
-board = ["   ","   ","   ","   ","   ","   "," O "," O "," O "] 
-
-def display_board(board)
-vert = "|"
-horiz = "-----------"
-  puts "#{board[0]}#{vert}#{board[1]}#{vert}#{board[2]}"
-  puts "#{horiz}"
-  puts "#{board[3]}#{vert}#{board[4]}#{vert}#{board[5]}"
-  puts "#{horiz}"
-  puts "#{board[6]}#{vert}#{board[7]}#{vert}#{board[8]}"
-end
 
