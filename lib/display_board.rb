3ee876ad9e374f0b44efed785f2ffb1fb83dd908
Chris Monzert <cmonzert@gmail.com>
2020-06-24 17:38:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@ board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 def display_board(board)
 vert = "|"
 horiz = "-----------"
-  puts "#{board[0]}#{vert}#{board[1]}#{vert}#{board[2]}"
+  puts " #{board[0]}#{vert}#{board[1]}#{vert}#{board[2]}"
   puts "#{horiz}"
   puts "#{board[3]}#{vert}#{board[4]}#{vert}#{board[5]}"
   puts "#{horiz}"
