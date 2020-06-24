459e68ccf386ea97bbad9e8dc1c07e2df15ef848
Chris Monzert <cmonzert@gmail.com>
2020-06-24 15:35:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 vert = "|"
 horiz = "-----------"
-row1 = board[0], vert, board[1], vert, board[2]
+row1 = "#{board[0]}, vert, board[1], vert, board[2]
 row2 = horiz
 row3 = board[3], vert, board[4], vert, board[5]
 row4 = horiz
